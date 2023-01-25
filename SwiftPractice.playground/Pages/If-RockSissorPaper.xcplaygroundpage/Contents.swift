c59b47491824c7d3d5ec92039e04bb75c30b45e5
AppleInTheTree <ejahn1175@gmail.com>
2023-01-25 08:39:21 +0700

    UPDATED IF-ROCK

--- SwiftPractice.playground/Pages/If-RockSissorPaper.xcplaygroundpage/Contents.swift
+++ SwiftPractice.playground/Pages/If-RockSissorPaper.xcplaygroundpage/Contents.swift
@@ -1,13 +1,49 @@
-import UIkit
+import UIKit
 
-var randomNumbers : Int = Int.random(in: 0...2)
 
-if randomNumbers == 0 {
+// 0 is Rock
+// 1 is Sissor
+// 2 is Paper
+
+var randomNumbers = Int.random(in: 0...2) //Number that computer choose
+var myNumber = Int.random(in: 0...2) // My number
+
+//printing the result of random
+switch randomNumbers{
+case 0:
     print("Rock")
-}
-else if randomNumbers == 1 {
+case 1:
     print("Sissor")
+case 2:
+    print("Paper")
+default: // Switch must be exhaustive
+    break
+}
+
+switch myNumber{
+case 0:
+    print("Rock")
+case 1:
+    print("Sissor")
+case 2:
+    print("Paper")
+default:
+    break
+}
+
+if myNumber == randomNumbers{
+    print("Draw")
+}
+else if myNumber == 0 && randomNumbers == 1  {
+    print("I won")
+}
+else if myNumber == 1 && randomNumbers == 2 {
+    print("I won")
+}
+else if myNumber == 2 && randomNumbers == 0{
+    print("I won")
 }
 else{
-    print("Paper")
+    print("I lose")
 }
+
