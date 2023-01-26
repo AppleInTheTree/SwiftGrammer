77ee5664133a8a4994c2db561dc30c3c343cad68
AppleInTheTree <ejahn1175@gmail.com>
2023-01-26 22:00:00 +0700

    [UPDATED] Part 6

--- Part6-Loops.playground/Pages/LoopPractice.xcplaygroundpage/Contents.swift
+++ Part6-Loops.playground/Pages/LoopPractice.xcplaygroundpage/Contents.swift
@@ -1,4 +1,15 @@
 import UIKit
+
+// Print the multiplation table
+
+for fisrt in 1...9 {
+    for second in 1...9 {
+        var result : Int = fisrt * second
+        print("\(fisrt) * \(second) = \(result)")
+    }
+}
+
+
 // Print the number that is mutiple of 3 in range of 1 through 100
 for i in 1...100 {
     if i % 3 == 0 {
@@ -8,13 +19,12 @@ for i in 1...100 {
 
 
 // Print the pyramid shape of emogi
-
-for i in 1...5 {
-    for a in (1...5) {
-        if a > i {
+for i in 1...5 { // 1 2
+    for a in (1...5) { // 1 2 1
+        if a > i { // 1 > 1 2 > 1
             continue
         }
-        print("🙂", terminator:"")
+        print("🙂", terminator:"") //🙂
     }
-    print()
+    print() // 1
 }
