4654ce14d3409c2275c547a79dbab19492567b07
AppleInTheTree <ejahn1175@gmail.com>
2023-01-30 22:01:26 +0700

    [UPDATED] Part7

--- Part7-Function.playground/Pages/FunctionPracticeThree.xcplaygroundpage/Contents.swift
+++ Part7-Function.playground/Pages/FunctionPracticeThree.xcplaygroundpage/Contents.swift
@@ -14,5 +14,24 @@ print(factorial(num: 4))
 
 // factorial using a recursive function
 
-func recursiveFunction(num: Int) -> Int {
+func recursiveFunction(num: Int) -> Int { 5
+    if num <= 1 {
+        return 1
+    }
+    
+    return num * recursiveFunction(num: num - 1)
 }
+
+print(recursiveFunction(num: 5))
+
+
+
+
+
+/*
+ 
+ re4 return 1   -> 1
+ re3 ( 2* 1 -> 2
+ re2 ( 3* 2 -> 6
+ re1 ( 4* 6 -> 24
+ */
