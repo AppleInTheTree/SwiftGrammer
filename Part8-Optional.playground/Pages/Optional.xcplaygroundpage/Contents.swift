771e79488cf8214e5bf5c7fd2bd23807df7d2a6e
AppleInTheTree <ejahn1175@gmail.com>
2023-02-02 15:01:30 +0700

    [UPDATED] Part8

--- Part8-Optional.playground/Pages/Optional.xcplaygroundpage/Contents.swift
+++ Part8-Optional.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -1,26 +1,51 @@
 import UIKit
 
-//var empty : Int
+var empty2 : Int
 
 // This will occur error because there is no value in that memory address
-//print(empty)
+//print(empty2)
 
 // This will not occuring the error -> optional type(some value called nill is packaging the value)
-var empty: Int? = 10 // (Temporal type)
+var empty: Int? // (Temporal type)
 print(empty)
 
-// If you want to bind non optional value in optional value -> It will be changed to optional type
+// If you bind non optional value in optional value -> It will be changed to optional type
+var empty3 = empty
+print(empty3) // empty3 is optional value
 
+// Can not calculate with optional value
+var a: Int? = 10
+var b: Int? = 11
+//a + b //this is error
 // If you dont unwrapped the optional type it will remain as optional type which is temporal type
 
+// There are 4 ways to unwrapp the optional type
+// 1.
+var number: Int? = 10
 
-
-if empty != nil {
-    print(empty!)
+print(number!)
+// 2.
+if number != nil {
+    print(number!)
 }
-
-if let a = empty {
-    print(a)
+// 3.
+if let num =  number {
+    print(num)
 }
+func guardLet(numbe: Int?) {
+    guard let num = numbe else{return}
+    print(num)
+}
+// 4. (nil Coalescing)
+// If you can set defalt value in optional type
+// If optional value has nil -> than print defalt value
+// severName has value "Danial", userName binds to "Danial
+var severName: String? = "Danial"
+var userName = severName ?? "Minjoo"
+
+// password has nil value -> pass is binding with defalt value which is 1234
+var password: Int?
+
+var pass = password ?? 1234
 
 
---
+++ Part8-Optional.playground/Pages/OptionalBindingExercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+
+var number: Int? = 7
+var hello: String? = "Hello"
+var water: String? = "Le Minerale"
+
+
+if let num = number {
+    print(num)
+}
+
+if let hi = hello {
+    print(hi)
+}
+
+func makeWater(){
+    guard let _ = water else {return}
+    print("New Water")
+}
+
+makeWater()
+
+// when passing optional as parameter, usually put nil value as a defalt value
+func saySomthing(word: String, num: Int? = nil){
+    guard let realNum =  num else {return}
+    print("\(word)","\(realNum)")
+}
+
+saySomthing(word: "hello", num: 5)
