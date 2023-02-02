0a947e6f8ddec96aad47e0743d5995017783f63b
AppleInTheTree <ejahn1175@gmail.com>
2023-02-02 14:03:18 +0700

    [UPDATED] Part8

---
+++ Part8-Optional.playground/Pages/IfLetBindingExercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+var number: Int? = 7
+var hello: String? = "Hello"
+
+print
+
+if let num = number {
+    print(num)
+}
+
+if let hi = hello {
+    print(hi)
+}
+
+
+// function에 옵셔널을 쓸 경우에 기본값으로 nil 값을 선언해논다
